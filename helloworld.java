4a34cc242be1216c178f9a2c37952d67027dacef
dk9410 <dk941012@gmail.com>
2018-03-07 23:46:39 +1100

    econd try

--- helloworld.java
+++ helloworld.java
@@ -1,8 +1,14 @@
-public class helloworld {
+//public class helloworld {
+//
+//    public static void main(String[] args) {
+//        // Prints "Hello, World" to the terminal window.
+//        System.out.println("Hello, World");
+//    }
+//
+//}
 
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+public class helloworld{
+    public static void main(String[] args){
+        System.out.println("Hello, Stark");
     }
-
 }
